90677ea11fc0ca4b1d61d35290bff048a5f70541
Herbet-filho <60247989+Herbet-filho@users.noreply.github.com>
2020-11-25 18:14:22 -0300

    Add files via upload

---
+++ 11.23.19.33 - estavel/petshop.hpp
@@ -0,0 +1,50 @@
+#ifndef _PETSHOP_
+#define _PETSHOP_
+
+#include <iostream>
+#include "animal.hpp"
+#include "veterinario.hpp"
+#include "tratador.hpp"
+#define CAPACIDADE_MAX_ANIMAL 100
+#define CAPACIDADE_MAX_VETERINARIO 10 
+#define CAPACIDADE_MAX_TRATADOR 10
+
+class petShop {
+    string nome;
+    Animal* animais[CAPACIDADE_MAX_ANIMAL];
+    int capacidade_animal = 0;
+    Veterinario* vets[CAPACIDADE_MAX_VETERINARIO];
+    int capacidade_veterinario = 0;
+    Tratador* trat[CAPACIDADE_MAX_TRATADOR];
+    int capacidade_tratador = 0;
+
+public:
+    
+    petShop();
+    petShop(string nome);
+    ~petShop();
+
+    string getNome();
+    void setNome(string nome);
+    
+    int getCapacidade_animal();   
+    int getCapacidade_veterinario();   
+    int getCapacidade_tratador();
+
+    void listarAnimais();
+    void listarVeterinarios();
+    void listarTratadores();
+
+    void addAnimal(Animal* animal_inserido);
+    void addVeterinario(Veterinario* veterinario_inserido);
+    void addTratador(Tratador* tratador_inserido);
+
+    void removerAnimal(string id_Animal);
+    void removerVeterinario(string id_Veterinario);
+    void removerTratador(string id_Tratador);
+
+    void consultarAnimal(string id_Animal);
+    void consultarVeterinario(string id_Veterinario);
+    void consultarTratador(string id_Tratador);
+};
+#endif
---
+++ 11.23.19.33 - estavel/test_tratador.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "animal.hpp"
+#include "funcionario.hpp"
+#include "tratador.hpp"
+#include "veterinario.hpp"
+#include "nivelSeguranca.hpp"
+
+using namespace std;
+
+int main(){
+    Tratador tratador();
+    Tratador trat1("fucrecio", "tratei123", 2780, 1);
+
+    cout << trat1.getNome() << endl;
+    cout << trat1.getId() << endl;
+    cout << trat1.getSalario() << endl;
+    cout << trat1.getNivel() << endl;
+
+
+
+    return 0;
+}
